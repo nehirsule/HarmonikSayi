21fc40a5a55fca8e114aebc9c1ba4634e4cbffc8
Şule Nehir ARAÇ <nehir.2706@gmail.com>
2024-03-08 11:42:24 +0300

    .

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Main {
-}
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bir Sayı Giriniz : ");
+        int sayi = input.nextInt();
+        System.out.println("N : "+ sayi);
+
+        double sonuc = 0.0;
+        for (double i = 1 ; i <= sayi; i++){
+            sonuc += 1/i;
+
+        }
+        System.out.println("N sayısının harmonik formülde karşılığı : " + sonuc);
+    }}
